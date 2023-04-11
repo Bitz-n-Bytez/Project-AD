373482fc1fb78458379261482bbba4a19f73fdd9
Maria Taskin Aumanee <128443071+mariaaumanee@users.noreply.github.com>
2023-04-11 12:36:58 +0800

    vvv

--- ride_sharing_app/lib/main.dart
+++ ride_sharing_app/lib/main.dart
@@ -9,6 +9,8 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
+  /maria
+  gyhu8jrgdfd
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
@@ -22,7 +24,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.amber,
+        primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
