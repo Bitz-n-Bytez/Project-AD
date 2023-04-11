a2ba8d63dd50d626ab6421b0e58e4b1874a07b09
Shafoora Naz Md Aslam <shafooranaz@gmail.com>
2023-04-11 12:30:52 +0800

    New

--- ride_sharing_app/lib/main.dart
+++ ride_sharing_app/lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        \cddcv
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
