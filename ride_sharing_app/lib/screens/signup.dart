a14140c58a26691bf4689dc357e7fa48e05b17f0
Nafis Abdullah <abdullahnafis20@gmail.com>
2023-04-13 03:44:45 +0800

    1st commit - SignUp Page
    Half SignUp page

---
+++ ride_sharing_app/lib/screens/signup.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+class LoginDriver extends StatefulWidget {
+  const LoginDriver({super.key});
+
+  @override
+  State<LoginDriver> createState() => _LoginDriverState();
+}
+
+class _LoginDriverState extends State<LoginDriver> {
+  bool _isHidden = true;
+
+  TextEditingController username = TextEditingController();
+  TextEditingController password = TextEditingController();
+  TextEditingController confirmPassword = TextEditingController();
+
+  @override
+  void initState() {
+    username.text = ""; //innitail value of text field
+    password.text = "";
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomCenter,
+              colors: <Color>[
+                Color.fromARGB(255, 4, 4, 4),
+                Color.fromARGB(255, 75, 74, 74),
+                Color.fromARGB(255, 167, 167, 165),
+              ], // Gradient from https://learnui.design/tools/gradient-generator.html
+              tileMode: TileMode.mirror,
+            )),
+            child: Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    logoWidget("images/logo.png"),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 5),
+                      child: TextField(
+                          controller: username,
+                          style: const TextStyle(
+                              color: Color.fromARGB(255, 254, 252, 252)),
+                          decoration: const InputDecoration(
+                            labelText: "Username",
+                            icon: Icon(
+                              Icons.people,
+                              color: Colors.white,
+                            ), //icon at head of input
+                          )),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 5),
+                      child: TextField(
+                          controller: password,
+                          style: const TextStyle(
+                              color: Color.fromARGB(255, 254, 252, 252)),
+                          obscureText: _isHidden,
+                          decoration: InputDecoration(
+                            icon: const Icon(
+                              Icons.lock,
+                              color: Colors.white,
+                            ), //icon at head of input
+                            //prefixIcon: Icon(Icons.people), //you can use prefixIcon property too.
+                            labelText: "Password",
+                            suffix: InkWell(
+                              onTap: _togglePasswordView,
+                              child: const Icon(
+                                Icons.visibility,
+                                color: Colors.white,
+                              ),
+                            ),
+                          )),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 5),
+                      child: TextField(
+                          controller: confirmPassword,
+                          style: const TextStyle(
+                              color: Color.fromARGB(255, 254, 252, 252)),
+                          obscureText: _isHidden,
+                          decoration: InputDecoration(
+                            icon: const Icon(
+                              Icons.lock,
+                              color: Colors.white,
+                            ), //icon at head of input
+                            //prefixIcon: Icon(Icons.people), //you can use prefixIcon property too.
+                            labelText: "Confirm Password",
+                            suffix: InkWell(
+                              onTap: _togglePasswordView,
+                              child: const Icon(
+                                Icons.visibility,
+                                color: Colors.white,
+                              ),
+                            ),
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+            )));
+  }
+
+  void _togglePasswordView() {
+    setState(() {
+      _isHidden = !_isHidden;
+    });
+  }
+}
+
+Image logoWidget(String imageName) {
+  return Image.asset(
+    imageName,
+    fit: BoxFit.fitWidth,
+    width: 200,
+    height: 200,
+  );
+}
