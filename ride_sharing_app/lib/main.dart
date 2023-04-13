1b0599273b777a8d2919840a2d6c22abac04bab2
Nafis Abdullah <abdullahnafis20@gmail.com>
2023-04-14 03:04:25 +0800

    Update main.dart

--- ride_sharing_app/lib/main.dart
+++ ride_sharing_app/lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:ride_sharing_app/screens/signup.dart';
 import 'package:flutter/material.dart';
-
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
