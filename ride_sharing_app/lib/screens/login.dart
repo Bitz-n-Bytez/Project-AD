b59d1b32687ed8e14051faa37c1ad347ea41bf13
Nafis Abdullah <abdullahnafis20@gmail.com>
2023-05-09 22:59:58 +0800

    Update login.dart

--- ride_sharing_app/lib/screens/login.dart
+++ ride_sharing_app/lib/screens/login.dart
@@ -51,133 +51,130 @@ class _LoginState extends State<Login> {
                 )),
             child: Center(
               child: SingleChildScrollView(
-                child: Column(
-                  children: <Widget>[
-                    logoWidget("images/logo.png"),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 5),
-                      child: TextField(
-                          controller: email,
-                          style: const TextStyle(
-                              color: Color.fromARGB(255, 254, 252, 252)),
-                          decoration: const InputDecoration(
-                            labelText: "Email",
-                            icon: Icon(
-                              Icons.people,
+                  child: Column(
+                children: <Widget>[
+                  logoWidget("images/logo.png"),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    child: TextField(
+                        controller: email,
+                        style: const TextStyle(
+                            color: Color.fromARGB(255, 254, 252, 252)),
+                        decoration: const InputDecoration(
+                          labelText: "Email",
+                          icon: Icon(
+                            Icons.people,
+                            color: Colors.white,
+                          ), //icon at head of input
+                        )),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    child: TextField(
+                        controller: password,
+                        style: const TextStyle(
+                            color: Color.fromARGB(255, 254, 252, 252)),
+                        obscureText: _isHidden,
+                        decoration: InputDecoration(
+                          icon: const Icon(
+                            Icons.lock,
+                            color: Colors.white,
+                          ), //icon at head of input
+                          //prefixIcon: Icon(Icons.people), //you can use prefixIcon property too.
+                          labelText: "Password",
+                          suffix: InkWell(
+                            onTap: _togglePasswordView,
+                            child: const Icon(
+                              Icons.visibility,
                               color: Colors.white,
-                            ), //icon at head of input
-                          )),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 5),
-                      child: TextField(
-                          controller: password,
-                          style: const TextStyle(
-                              color: Color.fromARGB(255, 254, 252, 252)),
-                          obscureText: _isHidden,
-                          decoration: InputDecoration(
-                            icon: const Icon(
-                              Icons.lock,
-                              color: Colors.white,
-                            ), //icon at head of input
-                            //prefixIcon: Icon(Icons.people), //you can use prefixIcon property too.
-                            labelText: "Password",
-                            suffix: InkWell(
-                              onTap: _togglePasswordView,
-                              child: const Icon(
-                                Icons.visibility,
-                                color: Colors.white,
+                            ),
+                          ),
+                        )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 50, 20, 10),
+                    child: SizedBox(
+                        child: CupertinoButton.filled(
+                            child: const FittedBox(
+                              child: Text(
+                                'Login',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w400, fontSize: 20),
                               ),
                             ),
-                          )),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 50, 20, 10),
-                      child: SizedBox(
-                          child: CupertinoButton.filled(
-                              child: const FittedBox(
-                                child: Text(
-                                  'Login',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w400,
-                                      fontSize: 20),
-                                ),
-                              ),
-                              onPressed: () {
-                                FirebaseAuth.instance
-                                    .signInWithEmailAndPassword(
-                                        email: email.text,
-                                        password: password.text)
-                                    .then((value) {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              const EmailVerificationPage()));
-                                }).onError((error, stackTrace) {
-                                  QuickAlert.show(
-                                    context: context,
-                                    type: QuickAlertType.error,
-                                    text: 'Check all the details again!',
-                                  );
-                                });
-                              })),
-                    ),
-                    Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                        child: TextButton(
-                          child: const Text(
-                            "Forget Password",
-                            style: TextStyle(
-                                color: Colors.grey,
-                                fontFamily: "Dubai",
-                                fontSize: 14),
-                          ),
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const ForgetPass()));
-                          },
-                        )),
-                    Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-                        child: TextButton(
-                          child: const Text(
-                            "New User? Sign Up now!",
-                            style: TextStyle(
-                                color: Colors.grey,
-                                fontFamily: "Dubai",
-                                fontSize: 14),
-                          ),
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        const SignUpDriver()));
-                          },
-                        )),
-                    Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 20, vertical: 0),
-                        child: SignInButton(
-                          Buttons.Google,
-                          text: "Sign in with Google",
-                          onPressed: () async {
-                            // ignore: use_build_context_synchronously
-                            await GoogleAuth().handleSignIn();
-                            // ignore: use_build_context_synchronously
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) =>
-                                    const EmailVerificationPage()));
-                          },
-                        )),
-                  ],
-                ),
-              ),
+                            onPressed: () {
+                              FirebaseAuth.instance
+                                  .signInWithEmailAndPassword(
+                                      email: email.text,
+                                      password: password.text)
+                                  .then((value) {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const EmailVerificationPage()));
+                              }).onError((error, stackTrace) {
+                                QuickAlert.show(
+                                  context: context,
+                                  type: QuickAlertType.error,
+                                  text: 'Check all the details again!',
+                                );
+                              });
+                            })),
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                      child: TextButton(
+                        child: const Text(
+                          "Forget Password",
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontFamily: "Dubai",
+                              fontSize: 14),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const ForgetPass()));
+                        },
+                      )),
+                  Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+                      child: TextButton(
+                        child: const Text(
+                          "New User? Sign Up now!",
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontFamily: "Dubai",
+                              fontSize: 14),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const SignUpDriver()));
+                        },
+                      )),
+                  Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 0),
+                      child: SignInButton(
+                        Buttons.Google,
+                        text: "Sign in with Google",
+                        onPressed: () async {
+                          // ignore: use_build_context_synchronously
+                          await GoogleAuth().handleSignIn();
+                          // ignore: use_build_context_synchronously
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) =>
+                                  const EmailVerificationPage()));
+                        },
+                      )),
+                ],
+              )),
             )));
   }
 
