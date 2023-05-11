0392a007fad2764d0066d2d267bf881c7f5bffb4
Tasmiah Sarif <tasmiahnayna@gmail.com>
2023-05-11 11:36:20 +0800

    Added profile Image

--- ride_sharing_app/lib/screens/user_profile.dart
+++ ride_sharing_app/lib/screens/user_profile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:ride_sharing_app/screens/google_auth.dart';
 import 'login.dart';
 
 class UserProfile extends StatefulWidget {
@@ -20,56 +21,30 @@ class _UserProfileState extends State<UserProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("images/bg.jpg"),
-                    opacity: 0.1,
-                    fit: BoxFit.cover),
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomCenter,
-                  colors: <Color>[
-                    Color.fromARGB(255, 4, 4, 4),
-                    Color.fromARGB(255, 75, 74, 74),
-                    Color.fromARGB(255, 167, 167, 165),
-                  ], // Gradient from https://learnui.design/tools/gradient-generator.html
-                  tileMode: TileMode.mirror,
-                )),
-            child: Center(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: <Widget>[
-                    logoWidget("images/logo.png"),
-                    Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 20, vertical: 5),
-                        child: Text(
-                            "Welcome ${FirebaseAuth.instance.currentUser!.displayName!}")),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 50, 20, 10),
-                      child: SizedBox(
-                          child: CupertinoButton.filled(
-                              child: const FittedBox(
-                                child: Text(
-                                  'Logout',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w400,
-                                      fontSize: 20),
-                                ),
-                              ),
-                              onPressed: () async {
-                                await GoogleSignIn().signOut();
-                                // ignore: use_build_context_synchronously
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => const Login()));
-                              })),
-                    ),
-                  ],
-                ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              logoWidget("images/tprofileImage.jpg"),
+              SizedBox(
+                width: 120,
+                height: 120,
               ),
-            )));
+              const SizedBox(height: 10),
+              Text(FirebaseAuth.instance.currentUser!.displayName!,
+                  style: Theme.of(context).textTheme.headlineMedium),
+              const SizedBox(height: 20),
+              SizedBox(
+                  width: 200,
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("Edit Profile",
+                          style: TextStyle(color: Colors.black)))),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
