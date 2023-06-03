b87f3eb432df5eee29876375c241195211c56c1f
Nafis Abdullah <abdullahnafis20@gmail.com>
2023-06-04 04:19:01 +0800

    Latest before sprint 3

--- ride_sharing_app/lib/screens/driver_ride_request.dart
+++ ride_sharing_app/lib/screens/driver_ride_request.dart
@@ -1,87 +1,182 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:ride_sharing_app/screens/showriderequest.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:quickalert/quickalert.dart';
+import 'package:ride_sharing_app/screens/driver_home.dart';
 
 class DriverRideRequest extends StatefulWidget {
-  const DriverRideRequest({super.key});
-
   @override
   State<DriverRideRequest> createState() => _DriverRideRequestState();
 }
 
 class _DriverRideRequestState extends State<DriverRideRequest> {
-  GoogleMapController? _mapController;
-  Set<Marker> _markers = {};
+  late GoogleMapController _mapController;
+  Set<Polyline> _polylines = {};
+
+  @override
+  void dispose() {
+    _mapController.dispose();
+    super.dispose();
+  }
+
+  // Function to update the status in Firestore
+  void updateRideStatus(String requestId, String newStatus) {
+    FirebaseFirestore.instance
+        .collection('rideRequests')
+        .doc(requestId)
+        .update({'status': newStatus}).then((value) {
+      // Status updated successfully
+      print('Status updated');
+    }).catchError((error) {
+      // Error occurred while updating status
+      print('Failed to update status: $error');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Driver Page'),
+        title: Text('Driver Ride Requests'),
       ),
       body: StreamBuilder<QuerySnapshot>(
         stream:
-            FirebaseFirestore.instance.collection('rideRequest').snapshots(),
+            FirebaseFirestore.instance.collection('rideRequests').snapshots(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            _markers.clear();
-            final rideOffers = snapshot.data!.docs;
-            for (var offer in rideOffers) {
-              final data = offer.data() as Map<String, dynamic>;
-              final pickupLatLng = data['pickup_lat_lng'] as GeoPoint?;
-              final destinationLatLng =
-                  data['destination_lat_lng'] as GeoPoint?;
+            final rideRequests = snapshot.data!.docs;
 
-              if (pickupLatLng != null && destinationLatLng != null) {
-                _markers.add(
-                  Marker(
-                    markerId: MarkerId(offer.id),
-                    position:
-                        LatLng(pickupLatLng.latitude, pickupLatLng.longitude),
-                    infoWindow: InfoWindow(
-                      title: 'Ride Offer',
-                      snippet:
-                          'Destination: ${destinationLatLng.latitude}, ${destinationLatLng.longitude}',
-                    ),
-                  ),
+            return ListView.builder(
+              itemCount: rideRequests.length,
+              itemBuilder: (context, index) {
+                final data = rideRequests[index].data() as Map<String, dynamic>;
+
+                final requestId = rideRequests[index].id; // Get the document ID
+                final userId = data['userId'] as String;
+                final pickup = data['pickup'] as GeoPoint;
+                final destination = data['destination'] as GeoPoint;
+                final distance = data['distance'] as double;
+                final duration = data['duration'] as double;
+                final fare = data['fare'] as double;
+                final status = data['status'] as String;
+
+                final pickupLatLng = LatLng(pickup.latitude, pickup.longitude);
+                final destinationLatLng =
+                    LatLng(destination.latitude, destination.longitude);
+
+                // Define the polyline options
+                final polyline = Polyline(
+                  polylineId: PolylineId('route'),
+                  color: Colors.blue,
+                  width: 4,
+                  points: [pickupLatLng, destinationLatLng],
                 );
-              }
-            }
 
-            return GoogleMap(
-              initialCameraPosition: CameraPosition(
-                target: LatLng(37.7749, -122.4194),
-                zoom: 12.0,
-              ),
-              onMapCreated: (controller) {
-                setState(() {
-                  _mapController = controller;
-                });
+                _polylines.add(polyline); // Add the polyline to the set
+
+                return Column(
+                  children: [
+                    ListTile(
+                      title: Text('User Email: $userId'),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                              'Pickup: (${pickup.latitude}, ${pickup.longitude})'),
+                          Text(
+                              'Destination: (${destination.latitude}, ${destination.longitude})'),
+                          Text('Distance: $distance'),
+                          Text('Duration: $duration'),
+                          Text('Fare: $fare'),
+                          Text('Status: $status'),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 200, // Adjust the height as needed
+                      child: GestureDetector(
+                        onPanUpdate: (details) {
+                          _mapController.animateCamera(
+                            CameraUpdate.scrollBy(
+                              -details.delta.dx,
+                              -details.delta.dy,
+                            ),
+                          );
+                        },
+                        child: GoogleMap(
+                          onMapCreated: (controller) {
+                            setState(() {
+                              _mapController = controller;
+                            });
+                          },
+                          markers: {
+                            Marker(
+                              markerId: MarkerId('pickup'),
+                              position: pickupLatLng,
+                              infoWindow: InfoWindow(title: 'Pickup'),
+                            ),
+                            Marker(
+                              markerId: MarkerId('destination'),
+                              position: destinationLatLng,
+                              infoWindow: InfoWindow(title: 'Destination'),
+                            ),
+                          },
+                          polylines: _polylines, // Set the polylines
+                          initialCameraPosition: CameraPosition(
+                            target: pickupLatLng,
+                            zoom: 15,
+                          ),
+                        ),
+                      ),
+                    ),
+                    ButtonBar(
+                      children: [
+                        ElevatedButton(
+                          onPressed: () async {
+                            // Accept the ride request
+                            updateRideStatus(requestId, 'Accepted');
+                            await QuickAlert.show(
+                              context: context,
+                              type: QuickAlertType.success,
+                              text: 'Ride Request Accepted!',
+                            );
+
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => const DriverHomePage(),
+                              ),
+                            );
+                          },
+                          child: Text('Accept'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () async {
+                            // Reject the ride request
+                            updateRideStatus(requestId, 'Rejected');
+                            await QuickAlert.show(
+                              context: context,
+                              type: QuickAlertType.error,
+                              text: 'Ride Request Rejected!',
+                            );
+
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => const DriverHomePage(),
+                              ),
+                            );
+                          },
+                          child: Text('Reject'),
+                        ),
+                      ],
+                    ),
+                  ],
+                );
               },
-              markers: _markers,
             );
           }
 
           return Center(child: CircularProgressIndicator());
         },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          // Handle accepting or rejecting the ride offer
-          final acceptedOfferId =
-              _markers.isNotEmpty ? _markers.first.markerId.value : '';
-          await FirebaseFirestore.instance
-              .collection('rideRequest')
-              .doc(acceptedOfferId)
-              .update({
-            'status': 'accepted',
-          });
-          // ignore: use_build_context_synchronously
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => const ShowRideRequest()));
-        },
-        child: Icon(Icons.check),
       ),
     );
   }
--- ride_sharing_app/lib/screens/mapscreen.dart
+++ ride_sharing_app/lib/screens/mapscreen.dart
@@ -3,9 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:geocoding/geocoding.dart' as geo;
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_google_places/flutter_google_places.dart';
+import 'package:google_maps_webservice/places.dart';
 import 'package:quickalert/quickalert.dart';
 import 'package:ride_sharing_app/screens/customer_home.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart' as poly;
+import 'package:intl/intl.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
@@ -23,6 +31,11 @@ class _MapScreenState extends State<MapScreen> {
   double _estimatedDistance = 0;
   double _estimatedDuration = 0;
   double _estimatedFare = 0;
+  final places =
+      GoogleMapsPlaces(apiKey: 'AIzaSyD-veSMr3-awR3JWG3uPDpQWc5HjvA03X0');
+  List<LatLng> _polylineCoordinates = [];
+  Set<Polyline> _polylines = {};
+  PolylinePoints _polylinePoints = PolylinePoints();
 
   @override
   void initState() {
@@ -33,9 +46,9 @@ class _MapScreenState extends State<MapScreen> {
   void _setInitialCameraPosition() {
     if (_mapController != null) {
       _mapController!.animateCamera(CameraUpdate.newCameraPosition(
-        CameraPosition(
-          target:
-              LatLng(1.4850, 103.7626), // KLG Campus Residence, Johor, Malaysia
+        const CameraPosition(
+          target: LatLng(
+              1.5555, 103.6382), // Universiti Teknologi Malaysia, Johor Bahru
           zoom: 15.0,
         ),
       ));
@@ -46,15 +59,15 @@ class _MapScreenState extends State<MapScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Google Maps Integration'),
+        title: const Text('Google Maps Integration'),
       ),
       body: Column(
         children: [
           Expanded(
             child: GoogleMap(
-              initialCameraPosition: CameraPosition(
+              initialCameraPosition: const CameraPosition(
                 target: LatLng(
-                    1.4850, 103.7626), // KLG Campus Residence, Johor, Malaysia
+                    1.5555, 103.6382), // KLG Campus Residence, Johor, Malaysia
                 zoom: 15.0,
               ),
               onMapCreated: (controller) {
@@ -66,6 +79,7 @@ class _MapScreenState extends State<MapScreen> {
                 _handleMapTap(latLng);
               },
               markers: _buildMarkers(),
+              polylines: _polylines,
             ),
           ),
           Padding(
@@ -78,53 +92,73 @@ class _MapScreenState extends State<MapScreen> {
                     labelText: 'Pickup Point',
                     labelStyle: TextStyle(color: Colors.black),
                   ),
+                  onTap: () async {
+                    Prediction? prediction = await PlacesAutocomplete.show(
+                      context: context,
+                      apiKey: 'AIzaSyD-veSMr3-awR3JWG3uPDpQWc5HjvA03X0',
+                      mode: Mode.overlay,
+                      language: 'en',
+                    );
+                    if (prediction != null) {
+                      PlacesDetailsResponse details =
+                          await places.getDetailsByPlaceId(prediction.placeId!);
+                      final lat = details.result.geometry!.location.lat;
+                      final lng = details.result.geometry!.location.lng;
+                      setState(() {
+                        _pickupLatLng = LatLng(lat, lng);
+                        _pickupController.text =
+                            details.result.formattedAddress!;
+                      });
+                    }
+                  },
                 ),
+                const SizedBox(height: 8.0),
                 TextField(
                   controller: _destinationController,
                   decoration: const InputDecoration(
-                    labelText: 'Destination Point',
+                    labelText: 'Destination',
                     labelStyle: TextStyle(color: Colors.black),
                   ),
+                  onTap: () async {
+                    Prediction? prediction = await PlacesAutocomplete.show(
+                      context: context,
+                      apiKey: 'AIzaSyD-veSMr3-awR3JWG3uPDpQWc5HjvA03X0',
+                      mode: Mode.overlay,
+                      language: 'en',
+                    );
+                    if (prediction != null) {
+                      PlacesDetailsResponse details =
+                          await places.getDetailsByPlaceId(prediction.placeId!);
+                      final lat = details.result.geometry!.location.lat;
+                      final lng = details.result.geometry!.location.lng;
+                      setState(() {
+                        _destinationLatLng = LatLng(lat, lng);
+                        _destinationController.text =
+                            details.result.formattedAddress!;
+                        _calculateEstimation();
+                        _createPolyline();
+                      });
+                    }
+                  },
                 ),
-                ElevatedButton(
-                  onPressed: _handleRideBooking,
-                  child: Text('Book Ride'),
-                ),
+                const SizedBox(height: 8.0),
                 Text('Estimated Distance: $_estimatedDistance km'),
+                const SizedBox(height: 8.0),
                 Text('Estimated Duration: $_estimatedDuration mins'),
-                Text('Estimated Fare: RM $_estimatedFare'),
+                const SizedBox(height: 8.0),
+                Text(
+                    'Estimated Fare: ${NumberFormat.currency(locale: 'en_MY', symbol: 'MYR').format(_estimatedFare)}'),
+                const SizedBox(height: 8.0),
+                ElevatedButton(
+                  onPressed: _requestRide,
+                  child: const Text('Request Ride'),
+                ),
               ],
             ),
           ),
         ],
       ),
     );
-  }
-
-  Set<Marker> _buildMarkers() {
-    Set<Marker> markers = {};
-
-    if (_pickupLatLng != null) {
-      markers.add(
-        Marker(
-          markerId: MarkerId('pickup'),
-          position: _pickupLatLng!,
-          infoWindow: InfoWindow(title: 'Pickup'),
-        ),
-      );
-    }
-
-    if (_destinationLatLng != null) {
-      markers.add(
-        Marker(
-          markerId: MarkerId('destination'),
-          position: _destinationLatLng!,
-          infoWindow: InfoWindow(title: 'Destination'),
-        ),
-      );
-    }
-
-    return markers;
   }
 
   void _handleMapTap(LatLng latLng) {
@@ -138,19 +172,71 @@ class _MapScreenState extends State<MapScreen> {
         _destinationLatLng = latLng;
         _destinationController.text = latLng.toString();
         _calculateEstimation();
+        _createPolyline();
       });
     }
   }
 
+  Set<Marker> _buildMarkers() {
+    final markers = <Marker>{};
+    if (_pickupLatLng != null) {
+      markers.add(
+        Marker(
+          markerId: const MarkerId('pickup'),
+          position: _pickupLatLng!,
+          icon:
+              BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+        ),
+      );
+    }
+    if (_destinationLatLng != null) {
+      markers.add(
+        Marker(
+          markerId: const MarkerId('destination'),
+          position: _destinationLatLng!,
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+        ),
+      );
+    }
+    return markers;
+  }
+
+  Future<Map<String, dynamic>> _fetchRouteDetails(
+      LatLng origin, LatLng destination) async {
+    final apiKey = 'AIzaSyD-veSMr3-awR3JWG3uPDpQWc5HjvA03X0';
+    final apiUrl =
+        'https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}&destination=${destination.latitude},${destination.longitude}&key=$apiKey';
+
+    final response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      if (data['status'] == 'OK') {
+        final route = data['routes'][0];
+        final leg = route['legs'][0];
+        final distance = leg['distance']['value']; // in meters
+        final duration = leg['duration']['value']; // in seconds
+
+        return {
+          'distance': distance,
+          'duration': duration,
+        };
+      }
+    }
+
+    return {
+      'distance': 0,
+      'duration': 0,
+    };
+  }
+
   void _calculateEstimation() async {
     if (_pickupLatLng != null && _destinationLatLng != null) {
-      double distance = await _calculateDistance(
-        _pickupLatLng!,
-        _destinationLatLng!,
-      );
-
-      double duration = _calculateDuration(distance);
-      double fare = _calculateFare(distance);
+      final routeDetails =
+          await _fetchRouteDetails(_pickupLatLng!, _destinationLatLng!);
+      final distance = routeDetails['distance'] / 1000; // Convert to kilometers
+      final duration = routeDetails['duration'] / 60; // Convert to minutes
+      final fare = distance * 1.75; // Assuming fare of MYR 1.5 per kilometer
 
       setState(() {
         _estimatedDistance = distance;
@@ -160,102 +246,67 @@ class _MapScreenState extends State<MapScreen> {
     }
   }
 
-  Future<double> _calculateDistance(
-    LatLng startLatLng,
-    LatLng endLatLng,
-  ) async {
-    List<Location> startLocations = await locationFromAddress(
-      _getAddressFromLatLng(startLatLng),
-    );
-    List<Location> endLocations = await locationFromAddress(
-      _getAddressFromLatLng(endLatLng),
-    );
-
-    LatLng startLocation = LatLng(0, 0); // Dummy initial value
-    LatLng endLocation = LatLng(0, 0); // Dummy initial value
-
-    if (startLocations.isNotEmpty) {
-      startLocation = LatLng(
-        startLocations.first.latitude,
-        startLocations.first.longitude,
-      );
-    }
-
-    if (endLocations.isNotEmpty) {
-      endLocation = LatLng(
-        endLocations.first.latitude,
-        endLocations.first.longitude,
-      );
-    }
-
-    double distance = await Geolocator.distanceBetween(
-      startLocation.latitude,
-      startLocation.longitude,
-      endLocation.latitude,
-      endLocation.longitude,
-    );
-
-    return distance / 1000; // Convert to kilometers
-  }
-
-  String _getAddressFromLatLng(LatLng latLng) {
-    return '${latLng.latitude},${latLng.longitude}';
-  }
-
-  double _calculateDuration(double distance) {
-    double averageSpeed = 80; // km/h
-    double duration = distance / averageSpeed * 60; // minutes
-    return duration;
-  }
-
-  double _calculateFare(double distance) {
-    double ratePerKm = 1.5; // Rate per kilometer in MYR
-    double fare = distance * ratePerKm;
-    return fare;
-  }
-
-  void _handleRideBooking() async {
+  void _createPolyline() async {
     if (_pickupLatLng != null && _destinationLatLng != null) {
-      // Save the booking details to Firestore or trigger any relevant actions
-      // You can use Firebase Firestore or any other backend service for data storage and retrieval
-      // In this example, we'll save the booking details to Firestore
-
-      String pickupAddress = await _getAddressFromLatLng(_pickupLatLng!);
-      String destinationAddress =
-          await _getAddressFromLatLng(_destinationLatLng!);
-
-      CollectionReference rideOffersRef =
-          FirebaseFirestore.instance.collection('rideRequest');
-
-      await rideOffersRef.add({
-        'pickup': pickupAddress,
-        'destination': destinationAddress,
-        'status': 'pending',
-        'userId': FirebaseAuth.instance.currentUser!.uid,
-      });
-
-      // ignore: use_build_context_synchronously
-      await QuickAlert.show(
-        context: context,
-        type: QuickAlertType.success,
-        text: 'Ride booked successfully!',
+      _polylines.clear();
+      _polylineCoordinates.clear();
+      PolylineResult result = await _polylinePoints.getRouteBetweenCoordinates(
+        'AIzaSyD-veSMr3-awR3JWG3uPDpQWc5HjvA03X0',
+        PointLatLng(_pickupLatLng!.latitude, _pickupLatLng!.longitude),
+        PointLatLng(
+            _destinationLatLng!.latitude, _destinationLatLng!.longitude),
+        travelMode: poly.TravelMode.driving,
       );
+      if (result.points.isNotEmpty) {
+        result.points.forEach((PointLatLng point) {
+          _polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+        });
 
-      // ignore: use_build_context_synchronously
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const CustomerHomePage(),
-        ),
-      );
+        setState(() {
+          Polyline polyline = Polyline(
+            polylineId: const PolylineId('polyline'),
+            color: Colors.blue,
+            points: _polylineCoordinates,
+            width: 3,
+          );
+          _polylines.add(polyline);
+        });
+      }
+    }
+  }
 
-      // Print the booking details
-      print('Ride booked from $_pickupLatLng to $_destinationLatLng');
-      print('Pickup Address: $pickupAddress');
-      print('Destination Address: $destinationAddress');
-      print('Estimated Distance: $_estimatedDistance km');
-      print('Estimated Duration: $_estimatedDuration mins');
-      print('Estimated Fare: RM $_estimatedFare');
+  void _requestRide() async {
+    if (_pickupLatLng != null && _destinationLatLng != null) {
+      FirebaseAuth auth = FirebaseAuth.instance;
+      User? user = auth.currentUser;
+
+      if (user != null) {
+        String userId = user.uid;
+
+        await FirebaseFirestore.instance.collection('rideRequests').add({
+          'userId': userId,
+          'pickup': GeoPoint(_pickupLatLng!.latitude, _pickupLatLng!.longitude),
+          'destination': GeoPoint(
+              _destinationLatLng!.latitude, _destinationLatLng!.longitude),
+          'distance': _estimatedDistance,
+          'duration': _estimatedDuration,
+          'fare': _estimatedFare,
+          'status': 'pending',
+        });
+
+        // ignore: use_build_context_synchronously
+        await QuickAlert.show(
+          context: context,
+          type: QuickAlertType.success,
+          text: 'Request Sent Successfully!',
+        );
+
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const CustomerHomePage(),
+          ),
+        );
+      }
     }
   }
 }
