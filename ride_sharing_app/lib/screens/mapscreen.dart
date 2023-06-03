caff480da5a62a8383f902f7a1360603e8675750
Nafis Abdullah <abdullahnafis20@gmail.com>
2023-06-03 17:56:51 +0800

    Map and Icon updated

--- ride_sharing_app/lib/screens/mapscreen.dart
+++ ride_sharing_app/lib/screens/mapscreen.dart
@@ -1,68 +1,223 @@
-// import 'dart:async';
-// import 'package:flutter/material.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-// class MapScreen extends StatefulWidget {
-//   final String driverId;
+class MapScreen extends StatefulWidget {
+  const MapScreen({Key? key}) : super(key: key);
 
-//   MapScreen({required this.driverId});
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
 
-//   @override
-//   _MapScreenState createState() => _MapScreenState();
-// }
+class _MapScreenState extends State<MapScreen> {
+  GoogleMapController? _mapController;
+  TextEditingController _pickupController = TextEditingController();
+  TextEditingController _destinationController = TextEditingController();
+  LatLng? _pickupLatLng;
+  LatLng? _destinationLatLng;
+  double _estimatedDistance = 0;
+  double _estimatedDuration = 0;
+  double _estimatedFare = 0;
 
-// class _MapScreenState extends State<MapScreen> {
-//   late GoogleMapController mapController;
-//   late StreamSubscription<DocumentSnapshot> locationSubscription;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Google Maps Integration'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: LatLng(37.7749, -122.4194),
+                zoom: 12.0,
+              ),
+              onMapCreated: (controller) {
+                setState(() {
+                  _mapController = controller;
+                });
+              },
+              onTap: (LatLng latLng) {
+                _handleMapTap(latLng);
+              },
+              markers: _buildMarkers(),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                TextField(
+                  controller: _pickupController,
+                  decoration: InputDecoration(
+                    labelText: 'Pickup Point',
+                  ),
+                ),
+                TextField(
+                  controller: _destinationController,
+                  decoration: InputDecoration(
+                    labelText: 'Destination Point',
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: _handleRideBooking,
+                  child: Text('Book Ride'),
+                ),
+                Text('Estimated Distance: $_estimatedDistance km'),
+                Text('Estimated Duration: $_estimatedDuration mins'),
+                Text('Estimated Fare: \$$_estimatedFare'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
-//   void _onMapCreated(GoogleMapController controller) {
-//     mapController = controller;
-//   }
+  Set<Marker> _buildMarkers() {
+    Set<Marker> markers = {};
 
-//   void _updateDriverMarker(LatLng position) {
-//     mapController.animateCamera(CameraUpdate.newLatLng(position));
-//   }
+    if (_pickupLatLng != null) {
+      markers.add(
+        Marker(
+          markerId: MarkerId('pickup'),
+          position: _pickupLatLng!,
+          infoWindow: InfoWindow(title: 'Pickup'),
+        ),
+      );
+    }
 
-//   void _listenToDriverLocation() {
-//     locationSubscription =
-//         _driversCollection.doc(widget.driverId).snapshots().listen((snapshot) {
-//       if (snapshot.exists) {
-//         final data = snapshot.data() as Map<String, dynamic>;
-//         final latitude = data['latitude'] as double;
-//         final longitude = data['longitude'] as double;
+    if (_destinationLatLng != null) {
+      markers.add(
+        Marker(
+          markerId: MarkerId('destination'),
+          position: _destinationLatLng!,
+          infoWindow: InfoWindow(title: 'Destination'),
+        ),
+      );
+    }
 
-//         final driverPosition = LatLng(latitude, longitude);
-//         _updateDriverMarker(driverPosition);
-//       }
-//     });
-//   }
+    return markers;
+  }
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     _listenToDriverLocation();
-//   }
+  void _handleMapTap(LatLng latLng) {
+    if (_pickupLatLng == null) {
+      setState(() {
+        _pickupLatLng = latLng;
+        _pickupController.text = latLng.toString();
+      });
+    } else if (_destinationLatLng == null) {
+      setState(() {
+        _destinationLatLng = latLng;
+        _destinationController.text = latLng.toString();
+        _calculateEstimation();
+      });
+    }
+  }
 
-//   @override
-//   void dispose() {
-//     locationSubscription.cancel();
-//     super.dispose();
-//   }
+  void _calculateEstimation() async {
+    if (_pickupLatLng != null && _destinationLatLng != null) {
+      double distance = await _calculateDistance(
+        _pickupLatLng!,
+        _destinationLatLng!,
+      );
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Driver Location'),
-//       ),
-//       body: GoogleMap(
-//         onMapCreated: _onMapCreated,
-//         initialCameraPosition: CameraPosition(
-//           target: LatLng(37.7749, -122.4194), // Set initial map center
-//           zoom: 12.0, // Set initial map zoom level
-//         ),
-//       ),
-//     );
-//   }
-// }
+      double duration = _calculateDuration(distance);
+      double fare = _calculateFare(distance);
+
+      setState(() {
+        _estimatedDistance = distance;
+        _estimatedDuration = duration;
+        _estimatedFare = fare;
+      });
+    }
+  }
+
+  Future<double> _calculateDistance(
+    LatLng startLatLng,
+    LatLng endLatLng,
+  ) async {
+    List<Location> startLocations = await locationFromAddress(
+      _getAddressFromLatLng(startLatLng),
+    );
+    List<Location> endLocations = await locationFromAddress(
+      _getAddressFromLatLng(endLatLng),
+    );
+
+    LatLng startLocation = LatLng(0, 0); // Dummy initial value
+    LatLng endLocation = LatLng(0, 0); // Dummy initial value
+
+    if (startLocations.isNotEmpty) {
+      startLocation = LatLng(
+        startLocations.first.latitude,
+        startLocations.first.longitude,
+      );
+    }
+
+    if (endLocations.isNotEmpty) {
+      endLocation = LatLng(
+        endLocations.first.latitude,
+        endLocations.first.longitude,
+      );
+    }
+
+    double distance = await Geolocator.distanceBetween(
+      startLocation.latitude,
+      startLocation.longitude,
+      endLocation.latitude,
+      endLocation.longitude,
+    );
+
+    return distance / 1000; // Convert to kilometers
+  }
+
+  String _getAddressFromLatLng(LatLng latLng) {
+    return '${latLng.latitude},${latLng.longitude}';
+  }
+
+  double _calculateDuration(double distance) {
+    double averageSpeed = 30; // km/h
+    double duration = distance / averageSpeed * 60; // minutes
+    return duration;
+  }
+
+  double _calculateFare(double distance) {
+    double ratePerKm = 1.5;
+    double fare = distance * ratePerKm;
+    return fare;
+  }
+
+  void _handleRideBooking() async {
+    if (_pickupLatLng != null && _destinationLatLng != null) {
+      // Save the booking details to Firestore or trigger any relevant actions
+      // You can use Firebase Firestore or any other backend service for data storage and retrieval
+      // In this example, we'll save the booking details to Firestore
+
+      String pickupAddress = await _getAddressFromLatLng(_pickupLatLng!);
+      String destinationAddress =
+          await _getAddressFromLatLng(_destinationLatLng!);
+
+      CollectionReference rideOffersRef =
+          FirebaseFirestore.instance.collection('rideRequest');
+
+      await rideOffersRef.add({
+        'pickup': pickupAddress,
+        'destination': destinationAddress,
+        'status': 'pending',
+        'userId': FirebaseAuth.instance.currentUser!.uid,
+      });
+
+      // Print the booking details
+      print('Ride booked from $_pickupLatLng to $_destinationLatLng');
+      print('Pickup Address: $pickupAddress');
+      print('Destination Address: $destinationAddress');
+      print('Estimated Distance: $_estimatedDistance km');
+      print('Estimated Duration: $_estimatedDuration mins');
+      print('Estimated Fare: \$$_estimatedFare');
+    }
+  }
+}
--- ride_sharing_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ride_sharing_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import firebase_core
 import firebase_storage
 import geolocator_apple
 import location
+import package_info_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -19,4 +20,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
 }
